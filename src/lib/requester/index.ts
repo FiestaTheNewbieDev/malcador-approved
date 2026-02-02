ef4067316391e5f3e4fb8181b0c1fef7f726a06f
Clément <clementfiesta@gmail.com>
2026-02-02 07:11:21 +0100

    refactor: Consolidate Axios request options with BASE_OPTIONS

--- src/lib/requester/index.ts
+++ src/lib/requester/index.ts
@@ -3,34 +3,68 @@ import { AxiosRequestConfig } from 'axios';
 import client from './client';
 import RequesterError from './error';
 
+const BASE_OPTIONS: AxiosRequestConfig = {
+  withCredentials: true,
+};
+
 const requester: Types.Requester = () => {
   const get: Types.Request = async <T>(
     url: string,
-    options?: AxiosRequestConfig,
-  ) => (await client.get<T>(url, options)).data;
+    options: AxiosRequestConfig = {},
+  ) =>
+    (
+      await client.get<T>(url, {
+        ...BASE_OPTIONS,
+        ...options,
+      })
+    ).data;
 
   const post: Types.RequestWithData = async <T>(
     url: string,
     data: unknown,
-    options?: AxiosRequestConfig,
-  ) => (await client.post<T>(url, data, options)).data;
+    options: AxiosRequestConfig = {},
+  ) =>
+    (
+      await client.post<T>(url, data, {
+        ...BASE_OPTIONS,
+        ...options,
+      })
+    ).data;
 
   const put: Types.RequestWithData = async <T>(
     url: string,
     data: unknown,
-    options?: AxiosRequestConfig,
-  ) => (await client.put<T>(url, data, options)).data;
+    options: AxiosRequestConfig = {},
+  ) =>
+    (
+      await client.put<T>(url, data, {
+        ...BASE_OPTIONS,
+        ...options,
+      })
+    ).data;
 
   const del: Types.Request = async <T>(
     url: string,
-    options?: AxiosRequestConfig,
-  ) => (await client.delete<T>(url, options)).data;
+    options: AxiosRequestConfig = {},
+  ) =>
+    (
+      await client.delete<T>(url, {
+        ...BASE_OPTIONS,
+        ...options,
+      })
+    ).data;
 
   const patch: Types.RequestWithData = async <T>(
     url: string,
     data: unknown,
-    options?: AxiosRequestConfig,
-  ) => (await client.patch<T>(url, data, options)).data;
+    options: AxiosRequestConfig = {},
+  ) =>
+    (
+      await client.patch<T>(url, data, {
+        ...BASE_OPTIONS,
+        ...options,
+      })
+    ).data;
 
   return { get, post, put, delete: del, patch };
 };
